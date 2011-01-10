74f0e62eef9227466b2454460c8d567ff890bb93
Carl Vondrick <cvondric@ics.uci.edu>
2011-01-09 17:22:12 -0800

    added cookie binding

--- turkic/public/javascript.js
+++ turkic/public/javascript.js
@@ -5,8 +5,8 @@ var turkic_timeaccepted = (new Date()).getTime();
 var scripts = document.getElementsByTagName("script");
 var src = scripts[scripts.length-1].src;
 var folder = src.substr(0, src.lastIndexOf('/'));
-
 document.write('<script src="' + folder + '/jquery.js"></script>');
+document.write('<script src="' + folder + '/jquery.cookie.js"></script>');
 })();
 
 function mturk_parameters()
