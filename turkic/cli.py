a2f63924a38a31314fa4f9e07c31734a1fd89e01
Carl Vondrick <cvondric@ics.uci.edu>
2011-01-12 15:34:32 -0800

    added donation option to hit groups

--- turkic/cli.py
+++ turkic/cli.py
@@ -51,7 +51,8 @@ class LoadCommand(object):
                         lifetime = args.lifetime,
                         cost = args.cost,
                         bonus = args.bonus,
-                        keywords = args.keywords)
+                        keywords = args.keywords,
+                        donate = args.donate)
         self(args, group)
 
     def __call__(self, args, group):
@@ -66,6 +67,7 @@ importparser.add_argument("--duration", type=int, default = 600)
 importparser.add_argument("--lifetime", type=int, default = 1209600)
 importparser.add_argument("--keywords", default = "")
 importparser.add_argument("--bonus", "-b", type=float, default = 0.00)
+importparser.add_argument("--donate", action="store_true")
 
 def main(args = None):
     """
--- turkic/server.py
+++ turkic/server.py
@@ -86,11 +86,84 @@ class Error404(Exception):
     def __init__(self, message):
         Exception.__init__(self, message)
 
-# bind some default handlers
-import serverutil
+
+
+
+import models
+import database
+from datetime import datetime
+
+def getworkerstats(hitid, workerid):
+    """
+    Returns the worker status as a dictionary for the server.
+    """
+    session = database.connect()
+    try:
+        status = {}
+
+        hit = session.query(models.HIT)
+        hit = hit.filter(models.HIT.hitid == hitid)
+        hit = hit.one()
+
+        status["reward"] = hit.group.cost
+        status["bonus"] = hit.group.bonus
+        status["donate"] = hit.group.donate
+        
+        worker = session.query(models.Worker)
+        worker = worker.filter(models.Worker.id == workerid)
+
+        try:
+            worker = worker.one()
+        except:
+            status["newuser"] = True
+        else:
+            status["newuser"] = False
+            status["numaccepted"] = worker.numacceptances
+            status["numrejected"] = worker.numrejections
+            status["numsubmitted"] = worker.numsubmitted
+        return status
+
+    finally:
+        session.close()
+
+def savejobstats(hitid, timeaccepted, timecompleted, donate, environ):
+    """
+    Saves statistics for a job.
+    """
+    session = database.connect()
+    try:
+        hit = session.query(models.HIT).filter(models.HIT.hitid == hitid).one()
+
+        hit.timeaccepted = datetime.fromtimestamp(int(timeaccepted) / 1000)
+        hit.timecompleted = datetime.fromtimestamp(int(timecompleted) / 1000)
+        hit.timeonserver = datetime.now()
+        hit.donatebonus = donate
+
+        hit.ipaddress = environ.get("HTTP_X_FORWARDED_FOR", None)
+        hit.ipaddress = environ.get("REMOTE_ADDR", hit.ipaddress)
+
+        session.add(hit)
+        session.commit()
+    finally:
+        session.close()
+
+def markcomplete(hitid, assignmentid, workerid):
+    """
+    Marks a job as complete. Usually this is called right before the
+    MTurk form is submitted.
+    """
+    session = database.connect()
+    try:
+        hit = session.query(models.HIT).filter(models.HIT.hitid == hitid).one()
+        hit.markcompleted(workerid, assignmentid)
+        session.add(hit)
+        session.commit()
+    finally:
+        session.close()
+
 handlers["turkic_getworkerstats"] = \
-    (serverutil.getworkerstats, "text/json", True, False, False)
+    (getworkerstats, "text/json", True, False, False)
 handlers["turkic_savejobstats"] = \
-    (serverutil.savejobstats, "text/json", True, False, True)
+    (savejobstats, "text/json", True, False, True)
 handlers["turkic_markcomplete"] = \
-    (serverutil.markcomplete, "text/json", True, False, False)
+    (markcomplete, "text/json", True, False, False)
